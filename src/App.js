681eb37c0aa265a205eb18af210fbb94ee3e7613
mos-au <m.fereidouni70@yahoo.com>
2024-04-10 10:01:10 +0330

    implement Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <AppRoutes />
       </div>
     </BrowserRouter>
