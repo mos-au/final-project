4823cf7cadeff590028e1a5e9d577972e7e12bb1
mos-au <m.fereidouni70@yahoo.com>
2024-04-10 09:51:43 +0330

    implement PostPage

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import FeedsPage from "./pages/FeedsPage";
+import PostPage from "./pages/PostPage/PostPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<FeedsPage />} />
-      <Route path="/post/:postId" element={<div>Post Page</div>} />
+      <Route path="/post/:postId" element={<PostPage />} />
       <Route path="*" element={<h1>404 Not Found!</h1>} />
     </Routes>
   );
---
+++ src/pages/PostPage/PostPage.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import Post from "../../components/Post/Post";
+import { useParams } from "react-router-dom";
+
+const PostPage = () => {
+  const [post, setPost] = useState(null);
+  const { postId } = useParams();
+
+  useEffect(() => {
+    getPost();
+  }, [postId]);
+
+  const getPost = async () => {
+    const postIdParam = postId ?? "";
+    const response = await fetch("http://localhost:5000/post/" + postIdParam);
+    const post = await response.json();
+
+    setPost(post);
+  };
+
+  if (!post) {
+    return <div>Loading....</div>;
+  }
+
+  return (
+    <div className="container">
+      <Post post={post} fullPage={true} />
+    </div>
+  );
+};
+
+export default PostPage;
